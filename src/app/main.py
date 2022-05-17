2e59aec404083d5d8c6e6a479e4ae3a23cfe8c01
rne1223 <rne1223@gmail.com>
2022-05-17 15:17:34 -0700

    Added sync support in skaffold, added debugging features in vscode, removed liveness probe and readiness probe from the deployment template.

--- src/app/main.py
+++ src/app/main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+import uvicorn
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+def read_root():
+    return {"Hello": "Hello World"}
 
+@app.get("/posts")
+def get_posts():
+    return {"data":"For real"}
 
+# @app.post("/createposts")
+# def create_posts(payload: dict = Body()):
+#     return {"message": "successfully created post foo"}
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Optional[str] = None):
 #     return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=8080, reload=True)
