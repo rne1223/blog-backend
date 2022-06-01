9cd0221cebc4a2e34ef292e3d6401f524b49c9f4
rne1223 <rne1223@gmail.com>
2022-06-01 02:33:59 -0700

    rebuilding project to use poetry

--- src/app/crud/crud_recipe.py
+++
@@ -1,8 +0,0 @@
-from crud.base import CRUDBase
-from models.recipe import Recipe
-from schemas.recipe import RecipeCreate, RecipeUpdate
-
-class CRUDrecipe(CRUDBase[Recipe, RecipeCreate, RecipeUpdate]):
-    pass
-
-recipe = CRUDrecipe(Recipe)
