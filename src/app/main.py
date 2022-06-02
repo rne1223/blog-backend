9929623f709fb88d8c435652fec6077f82ddafff
rne1223 <rne1223@gmail.com>
2022-06-02 00:25:07 -0700

    Changed recipe endpoints to blog endpoints

--- src/app/main.py
+++ src/app/main.py
@@ -1,94 +1,19 @@
-from fastapi import FastAPI, APIRouter, Query, HTTPException, Request, Depends
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, APIRouter, Query, HTTPException, Depends
 
 from typing import Optional, Any
 from pathlib import Path
 from sqlalchemy.orm import Session
-from app.recipe_data import BLOGS
-
-from app.schemas.recipe import RecipeSearchResults, Recipe, RecipeCreate
 from app.schemas.blog import BlogSearchResults, Blog, BlogCreate
 from app import deps
 from app import crud
 
-
 # Project Directories
 ROOT = Path(__file__).resolve().parent.parent
 BASE_PATH = Path(__file__).resolve().parent
-TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
-
-app = FastAPI(title="Recipe API", openapi_url="/openapi.json")
+app = FastAPI(title="Blog API", openapi_url="/openapi.json")
 
 api_router = APIRouter()
-
-
-# Updated to serve a Jinja2 template
-# https://www.starlette.io/templates/
-# https://jinja.palletsprojects.com/en/3.0.x/templates/#synopsis
-@api_router.get("/", status_code=200)
-def root(
-    request: Request,
-    db: Session = Depends(deps.get_db),
-) -> dict:
-    """
-    Root GET
-    """
-    recipes = crud.recipe.get_multi(db=db, limit=10)
-    return TEMPLATES.TemplateResponse(
-        "index.html",
-        {"request": request, "recipes": recipes},
-    )
-
-
-@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
-def fetch_recipe(
-    *,
-    recipe_id: int,
-    db: Session = Depends(deps.get_db),
-) -> Any:
-    """
-    Fetch a single recipe by ID
-    """
-    result = crud.recipe.get(db=db, id=recipe_id)
-    if not result:
-        # the exception is raised, not returned - you will get a validation
-        # error otherwise.
-        raise HTTPException(
-            status_code=404, detail=f"Recipe with ID {recipe_id} not found"
-        )
-
-    return result
-
-
-@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
-def search_recipes(
-    *,
-    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
-    max_results: Optional[int] = 10,
-    db: Session = Depends(deps.get_db),
-) -> dict:
-    """
-    Search for recipes based on label keyword
-    """
-    recipes = crud.recipe.get_multi(db=db, limit=max_results)
-    if not keyword:
-        return {"results": recipes}
-
-    results = filter(lambda recipe: keyword.lower() in recipe.label.lower(), recipes)
-    return {"results": list(results)[:max_results]}
-
-
-@api_router.post("/recipe/", status_code=201, response_model=Recipe)
-def create_recipe(
-    *, recipe_in: RecipeCreate, db: Session = Depends(deps.get_db)
-) -> dict:
-    """
-    Create a new recipe in the database.
-    """
-    recipe = crud.recipe.create(db=db, obj_in=recipe_in)
-
-    return recipe
 
  ## Blog endpoints
 @api_router.get("/blog/{blog_id}", status_code=200, response_model=Blog)
@@ -106,10 +31,38 @@ def fetch_blog(
         # the exception is raised, not returned - you will get a validation
         # error otherwise.
         raise HTTPException(
-            status_code=404, detail=f"Recipe with ID {blog_id} not found"
+            status_code=404, detail=f"Blog with ID {blog_id} not found"
         )
 
     return result
+
+@api_router.get("/search/", status_code=200, response_model=BlogSearchResults)
+def search_blogs(
+    *,
+    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
+    max_results: Optional[int] = 10,
+    db: Session = Depends(deps.get_db),
+) -> dict:
+    """
+    Search for recipes based on label keyword
+    """
+    blogs = crud.blog.get_multi(db=db, limit=max_results)
+    if not keyword:
+        return {"results": blogs}
+
+    results = filter(lambda blog: keyword.lower() in blog.title.lower(), blogs)
+    return {"results": list(results)[:max_results]}
+
+@api_router.post("/blog/", status_code=201, response_model=Blog)
+def create_recipe(
+    *, blog_in: BlogCreate, db: Session = Depends(deps.get_db)
+) -> dict:
+    """
+    Create a new blog in the database.
+    """
+    blog = crud.blog.create(db=db, obj_in=blog_in)
+
+    return blog 
 
 app.include_router(api_router)
 
@@ -117,5 +70,4 @@ app.include_router(api_router)
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+    uvicorn.run('main:app', host="0.0.0.0", port=8001, log_level="debug", reload=True)
